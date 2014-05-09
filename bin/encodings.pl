074ce016de3c302ad3a54b71e59f02c44aeb5657
Wakaba <wakaba@suikawiki.org>
2014-05-09 21:38:28 +0900

    Added encoding mapping data for <meta charset> (c.f. HTML revision 8618)

--- bin/encodings.pl
+++ bin/encodings.pl
@@ -1,14 +1,14 @@
 use strict;
 use warnings;
-use Path::Class;
-use lib glob file (__FILE__)->dir->subdir ('modules', '*', 'lib')->stringify;
-use JSON::PS qw(perl2json_bytes_for_record file2perl);
+use Path::Tiny;
+use lib glob path (__FILE__)->parent->child ('modules', '*', 'lib')->stringify;
+use JSON::PS;
 
 my $Data = {};
 
 {
-  my $f = file (__FILE__)->dir->parent->file ('local', 'encodings.json');
-  my $json = file2perl $f;
+  my $path = path (__FILE__)->parent->parent->child ('local/encodings.json');
+  my $json = json_bytes2perl $path->slurp;
   for (@$json) {
     for (@{$_->{encodings}}) {
       my $name = $_->{name};
@@ -19,18 +19,26 @@ my $Data = {};
   }
 }
 
+sub _name ($) {
+  return $Data->{supported_labels}->{lc $_[0]} || lc $_[0];
+} # _name
+
 {
-  my $f = file (__FILE__)->dir->parent->file
+  my $path = path (__FILE__)->parent->parent->child
       ('src', 'locale-default-encodings.txt');
-  for (($f->slurp)) {
+  for (split /\x0D?\x0A/, $path->slurp) {
     if (/^(\S+)\s+(\S+)$/) {
       my $label = lc $2;
-      $Data->{locale_default}->{lc $1} = $Data->{supported_labels}->{$label} || $label;
+      $Data->{locale_default}->{lc $1} = _name $label;
     } elsif (/\S/) {
       die "Broken data |$_|";
     }
   }
 }
+
+$Data->{html_decl_map}->{_name 'utf-16be'} = _name 'utf-8';
+$Data->{html_decl_map}->{_name 'utf-16le'} = _name 'utf-8';
+$Data->{html_decl_map}->{_name 'x-user-defined'} = _name 'windows-1252';
 
 print perl2json_bytes_for_record $Data;
 
