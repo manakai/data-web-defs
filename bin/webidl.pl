ca3f0448ee13ee78d22aedb7649febf78fe641a0
Wakaba <wakaba@suikawiki.org>
2015-11-22 19:55:21 +0900

    FrozenArray<> (WebIDL 775eace1758c99501cb52942c321c0c015a1b4bf)

--- bin/webidl.pl
+++ bin/webidl.pl
@@ -27,6 +27,7 @@ for (qw(
   Promise Error DOMException ArrayBuffer Int8Array Int16Array
   Int32Array Uint8Array Uint16Array Uint32Array Uint8ClampedArray
   Float32Array Float64Array DataView USVString
+  FrozenArray
 
   class extends
 
